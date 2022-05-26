cfadf706454df772b5de0bf6415c8e6632d902ad
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-26 12:31:56 -0300

    listas e objetos

---
+++ objetos.js
@@ -0,0 +1,9 @@
+const listaCPF = ["98767845876", "12345678909", "35946856890"]
+    //const cliente = ["nome", "Marinela", "idade", 36]
+
+const cliente = {
+    nome: "Marinela",
+    idade: 36,
+    cpf: "98767845876",
+    email: "marinela@teste.com"
+}
