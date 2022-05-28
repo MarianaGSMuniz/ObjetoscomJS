9922bb68aae8eefe91d23770aa405776e406e6d4
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-28 16:21:36 -0300

    continuação do desafio|

---
+++ infoclt.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Marinela",
+    idade: 36,
+    cpf: "98767845876",
+    email: "marinela@teste.com"
+}
+console.log('meu nome é ${cliente.nome} e tenho $ {cliente.idade} anos')
+console.log(cliente.cpf.substring(0, 3))
