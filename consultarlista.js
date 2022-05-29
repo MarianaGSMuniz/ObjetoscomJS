f5995ac4d80396a906cf7a4091b9c704a143fcd5
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-29 12:27:32 -0300

    chaves e colchetes

---
+++ consultarlista.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "Marinela",
+    idade: 36,
+    cpf: "98767845876",
+    email: "marinela@teste.com"
+}
+
+const chaves = ["nome", "idade", "cpf", "email"]
+
+console.log(chaves[0], )
