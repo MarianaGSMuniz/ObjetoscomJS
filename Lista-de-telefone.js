f86dd8fa4cacde57c76963810383c7b4f7c43041
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-30 13:29:33 -0300

    acessando listas

---
+++ Lista-de-telefone.js
@@ -0,0 +1,11 @@
+const cliente = {
+    nome: "Marinela",
+    idade: 36,
+    cpf: "98767845876",
+    email: "marinela@teste.com",
+    fones: ["123456987", "987654321"]
+
+}
+
+
+cliente.fones.forEach(fone => console.log(fone))
