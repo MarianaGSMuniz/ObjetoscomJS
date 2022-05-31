57f1e179cfe907aecefce084c059e8bc15b647c4
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-05-31 17:50:45 -0300

    compondo objeto

---
+++ compondo-objeto.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "Marinela",
+    idade: 36,
+    cpf: "98767845876",
+    email: "marinela@teste.com",
+    fones: ["123456987", "987654321"]
+
+}
+
+cliente.depedentes = {
+    nome: "Arendeu",
+    parentesco: "filho",
+    dataNasc: "01/02/2000"
+}
+
+console.log(cliente)
