926dd0453d7c4ee710baf75e79d66de0e6ce8fa2
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-01 20:26:47 -0300

    object manipulation

---
+++ manipulando.js
@@ -0,0 +1,28 @@
+const cliente = {
+    nome: "Leandro",
+    idade: 36,
+    email: "leandro@email.com",
+    telefones: ["+5511098765432", "+551309876579"]
+}
+cliente.animalEstimacao1 = [{
+    nome: "Tica",
+    raça: "Cão",
+    vacinado: true
+}]
+cliente.animalEstimacao1 = [{
+    nome: "Lua",
+    raça: "Gato",
+    vacinado: true
+}]
+
+cliente.animalEstimacao.push({
+    nome: "Miagui",
+    raça: "Gato",
+    vacinado: false
+})
+
+cliente.animalEstimacao.push({
+    nome: "Mingal",
+    raça: "Gato",
+    vacinado: false
+})
