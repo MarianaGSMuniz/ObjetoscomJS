abb2547074643e24182e1f19a56b5660d1565053
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-09 19:22:18 -0300

    propriedades

---
+++ cliente-generico.js
@@ -0,0 +1,9 @@
+function Cliente(nome, cpf, email, saldo) {
+    this.nome = nome
+    this.cpf = cpf
+    this.email = email
+    this.saldo = saldo
+    this.depositar = function(valor) {
+        this.saldo += valor
+    }
+}
