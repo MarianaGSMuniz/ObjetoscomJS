29270b25121d433bfe2d5f15dbdd6988a350489a
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-09 21:17:47 -0300

    class

---
+++ classe-cliente.js
@@ -0,0 +1,17 @@
+class Cliente {
+    constructor(nome, email, cpf, saldo) {
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+}
+depositar(valor); {
+    this.saldo += valor
+}
+
+exibirSaldo(); {
+    console.log(this.saldo)
+}
+
+const mari = new Cliente("Mari", "mari@email.com", "1478624898412", 100)
